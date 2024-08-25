ac38dd47af4f17d1f22fc127293363409b428128
kartik-soni1707 <its.kartiksoni@gmail.com>
2024-08-25 13:51:16 -0700

    setting up FE for react

--- music_app/music_app/urls.py
+++ music_app/music_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('apis/', include('API.urls'))
+    path('apis/', include('API.urls')),
+    path('', include('frontend.urls'))
 ]
